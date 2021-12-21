8af88b085f8cf070b3c890c60cad63ba7e623638
drjadhav26 <68788956+drjadhav26@users.noreply.github.com>
2021-12-21 13:42:44 +0530

    Update application.py

--- application.py
+++ application.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     # Create data for the home page to pass the Maps key.
-    data = { "map_key" : MAP_KEY }
+    data = { "map_key" : MAP_KEY, "map_key1" : WAQI_API_KEY1}
     # Return the rendered HTML page
     return render_template("home.html", data = data)
 
